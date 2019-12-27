bf8247ad26e189ae56bd2e68bcf724b467b6be8b
Steven Schwenke <steven@stevenschwenke.de>
2019-12-27 10:02:49 +0100

    Add groupsUrl to load groups from new endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
   adminUrl: 'https://ithubbsbackend.cfapps.io/ithubbs/api/admin',
   adminEventsUrl: 'https://ithubbsbackend.cfapps.io/ithubbs/api/admin/events',
   adminGroupsUrl: 'https://ithubbsbackend.cfapps.io/ithubbs/api/admin/groups',
+  groupsUrl: 'https://ithubbsbackend.cfapps.io/ithubbs/api/groups',
 
   eventsUrl: 'https://ithubbsbackend.cfapps.io/ithubbs/api/events'
 };
